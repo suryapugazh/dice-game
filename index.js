5e6194ed4bf0b08a63485a7048cb0224e1fc08af
Surya <suryapugazh27@gmail.com>
2025-06-21 20:50:39 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,16 @@
+var randomNumber1 = Math.random();
+randomNumber1 = Math.floor(randomNumber1 * 6) + 1;
+
+var randomNumber2 = Math.random()
+randomNumber2 = Math.floor(randomNumber2 * 6) + 1;
+
+document.querySelectorAll("img")[0].setAttribute("src", "./images/dice" + randomNumber1 + ".png");
+document.querySelectorAll("img")[1].setAttribute("src", "./images/dice" + randomNumber2 + ".png");
+
+if(randomNumber1 > randomNumber2){
+    document.querySelector("h1").textContent = "🚩 Player 1 Wins!";
+} else if(randomNumber2 > randomNumber1){
+    document.querySelector("h1").textContent = "Player 2 Wins! 🚩";
+} else {
+    document.querySelector("h1").textContent = "Draw!";
+}
